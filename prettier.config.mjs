e4c4e0b57b2822db6e174cae8a48a2848f318e34
Tomas Zaluckij <me@tomaszal.com>
2024-06-24 12:23:58 +0100

    feat: init plasmo project

---
+++ prettier.config.mjs
@@ -0,0 +1,29 @@
+/**
+ * @see https://prettier.io/docs/en/options
+ * @type {import('prettier').Options}
+ */
+export default {
+  printWidth: 120,
+
+  singleQuote: true,
+  jsxSingleQuote: true,
+
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+
+  /**
+   * @see https://github.com/IanVS/prettier-plugin-sort-imports?tab=readme-ov-file#importorder
+   * @type {import('@ianvs/prettier-plugin-sort-imports').PluginConfig['importOrder']}
+   */
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@plasmo/(.*)$',
+    '',
+    '^@plasmohq/(.*)$',
+    '',
+    '^@/(.*)$',
+    '',
+    '^[./]',
+  ],
+};
---
+++ src/popup.tsx
@@ -0,0 +1,5 @@
+const PopupPage = () => {
+  return <div>Hello World</div>;
+};
+
+export default PopupPage;
