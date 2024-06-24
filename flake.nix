5e769c224a87eed8c7837b9610edfc0a2d08129e
Tomas Zaluckij <me@tomaszal.com>
2024-06-24 13:48:32 +0100

    feat: add nix flake for reproducible dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Follows
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          NIX_PATH = ["nixpkgs=${inputs.nixpkgs}"];
+          nativeBuildInputs = with pkgs; [
+            # Nix tools
+            alejandra
+            nixd
+
+            # JS tools
+            nodejs
+            nodePackages.pnpm
+          ];
+        };
+      };
+    };
+}
