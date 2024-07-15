c9be7e05f18d4a8c35f6f6cd97c947d4cb56bd36
Tomas Zaluckij <me@tomaszal.com>
2024-07-15 15:17:10 +0100

    feat: enable dev env on any all nix compatible systems

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
 
     # Follows
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -9,7 +10,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "x86_64-darwin"];
+      systems = import inputs.systems;
       perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShell {
           NIX_PATH = ["nixpkgs=${inputs.nixpkgs}"];
