64be89ade0128b8e1727e72e57200375488cc2fc
Tomas Zaluckij <me@tomaszal.com>
2024-07-16 15:42:09 +0100

    ci: implement ci checks

--- flake.nix
+++ flake.nix
@@ -11,22 +11,51 @@
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        lib,
+        pkgs,
+        ...
+      }: let
+        package = lib.importJSON ./package.json;
+        inherit (package) version;
+        pname = package.name;
+        src = ./.;
+
+        taskInputs = with pkgs; [
+          # JS tools
+          nodejs
+          pnpm_9
+
+          # Other
+          cocogitto
+          go-task
+        ];
+      in {
+        legacyPackages = pkgs;
+
         devShells.default = pkgs.mkShell {
           NIX_PATH = ["nixpkgs=${inputs.nixpkgs}"];
-          nativeBuildInputs = with pkgs; [
-            # Nix tools
-            alejandra
-            nixd
 
-            # JS tools
-            nodejs
-            nodePackages.pnpm
+          nativeBuildInputs =
+            taskInputs
+            ++ (with pkgs; [
+              # Nix tools
+              alejandra
+              nixd
+            ]);
+        };
 
-            # Other
-            cocogitto
-            go-task
-          ];
+        checks.lint = pkgs.stdenv.mkDerivation {
+          inherit version src;
+          pname = "${pname}-check-lint";
+          pnpmDeps = pkgs.pnpm_9.fetchDeps {
+            inherit pname version src;
+            hash = "sha256-zqa7lEbk/9QNFZIbYrAWfVFKpwGNq1umwobNgCf1alk=";
+          };
+          nativeBuildInputs = taskInputs ++ (with pkgs; [pnpm_9.configHook]);
+          doCheck = true;
+          checkPhase = "task lint COG=false";
+          installPhase = "touch $out";
         };
       };
     };
