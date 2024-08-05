79676a4bcf08df61e03e57fc98332d2643c96b62
Tomas Zaluckij <me@tomaszal.com>
2024-08-05 13:09:21 +0100

    Implement Postman collection import

--- apps/saas/src/collection.tsx
+++ apps/saas/src/collection.tsx
@@ -1,6 +1,8 @@
-import { useMutation, useQuery } from '@connectrpc/connect-query';
+import { createConnectQueryKey, useMutation, useQuery } from '@connectrpc/connect-query';
+import { useQueryClient } from '@tanstack/react-query';
 import { getRouteApi, Link } from '@tanstack/react-router';
 import { flow, Match, Struct } from 'effect';
+import { Button, FileTrigger } from 'react-aria-components';
 
 import { ItemApiCall } from '@the-dev-tools/protobuf/collection/v1/collection_pb';
 import * as CollectionQuery from '@the-dev-tools/protobuf/collection/v1/collection-CollectionService_connectquery';
@@ -13,6 +15,7 @@ export const CollectionListPage = () => {
   return (
     <>
       <h2 className='text-center text-2xl font-extrabold'>Collections</h2>
+      <ImportPostman />
       <div>
         {collections.map((_) => (
           <Link key={_.id} to='/collection/$id' params={{ id: _.id }}>
@@ -21,6 +24,32 @@ export const CollectionListPage = () => {
         ))}
       </div>
     </>
+  );
+};
+
+const ImportPostman = () => {
+  const queryClient = useQueryClient();
+  const createMutation = useMutation(CollectionQuery.importPostman);
+
+  return (
+    <div>
+      <span>Import Postman collection: </span>
+      <FileTrigger
+        onSelect={async (_) => {
+          const file = _?.item(0);
+          if (!file) return;
+          await createMutation.mutateAsync({
+            name: file.name,
+            data: new Uint8Array(await file.arrayBuffer()),
+          });
+          await queryClient.invalidateQueries({
+            queryKey: createConnectQueryKey(CollectionQuery.listCollections),
+          });
+        }}
+      >
+        <Button>Select a file</Button>
+      </FileTrigger>
+    </div>
   );
 };
 
