f0dd6a78da1c0e1807fc5ac47a47354c51e78d60
ElecTwix <root@electwix.dev>
2024-08-19 15:46:34 +0300

    base email impl

---
+++ dev-tools-mail/pkg/emailclient/emailclient.go
@@ -0,0 +1,82 @@
+package emailclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
+)
+
+var GlobalEmailClient *EmailClient
+
+const (
+	DefaultEmailFrom = "verify@dev.tools"
+)
+
+type EmailClient struct {
+	sesClient *sesv2.Client
+}
+
+const (
+	AWSRegionFrankfurt = "eu-central-1"
+)
+
+func NewClient(accessKey, secretKey, session string) (*EmailClient, error) {
+	if accessKey == "" || secretKey == "" {
+		return nil, fmt.Errorf("accessKey or secretKey is empty")
+	}
+
+	cred := credentials.NewStaticCredentialsProvider(accessKey, secretKey, session)
+
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithCredentialsProvider(cred), config.WithRegion("eu-central-1"))
+	if err != nil {
+		return nil, err
+	}
+
+	client := sesv2.NewFromConfig(cfg)
+
+	emailClient := &EmailClient{
+		sesClient: client,
+	}
+
+	GlobalEmailClient = emailClient
+	return emailClient, nil
+}
+
+func SendEmailText(client *EmailClient, subject, data, from string, recipients []string) (*sesv2.SendEmailOutput, error) {
+	// Text body or HTML body
+	body := &types.Content{
+		Data: &data,
+	}
+
+	// Email content structure
+	emailContent := &types.Message{
+		Body: &types.Body{
+			Text: body,
+		},
+		Subject: &types.Content{
+			Data: &subject,
+		},
+	}
+
+	// Send the email
+	emailInput := &sesv2.SendEmailInput{
+		FromEmailAddress: &from,
+		Destination: &types.Destination{
+			ToAddresses: recipients,
+		},
+		Content: &types.EmailContent{
+			Simple: emailContent,
+		},
+	}
+
+	output, err := client.sesClient.SendEmail(context.Background(), emailInput)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
