872ffd3e3cf4387aac52ec935d16c29055188270
Tomas Zaluckij <me@tomaszal.com>
2024-08-20 20:58:27 +0100

    Implement changeset workflow and CI

--- flake.nix
+++ flake.nix
@@ -1,37 +1,44 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    gha-nix-develop.url = "github:nicknovitski/nix-develop";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
 
     # Follows
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    gha-nix-develop.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
-      perSystem = {pkgs, ...}: let
-        runnerBuildInputs = with pkgs; [
-          # JS tools
-          nodejs
-          pnpm_9
-        ];
-      in {
+      perSystem = {
+        inputs',
+        pkgs,
+        self',
+        ...
+      }: {
+        packages.gha-nix-develop = inputs'.gha-nix-develop.packages.default;
+
+        devShells.runner = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # JS tools
+            nodejs
+            pnpm_9
+          ];
+        };
+
         devShells.default = pkgs.mkShell {
           NIX_PATH = ["nixpkgs=${inputs.nixpkgs}"];
 
           nativeBuildInputs =
-            runnerBuildInputs
+            self'.devShells.runner.nativeBuildInputs
             ++ (with pkgs; [
               # Nix tools
               alejandra
               nixd
             ]);
-        };
-
-        devShells.runner = pkgs.mkShell {
-          nativeBuildInputs = runnerBuildInputs;
         };
       };
     };
