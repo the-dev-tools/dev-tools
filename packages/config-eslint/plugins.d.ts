b314724674faf0d2aae41c7fbe1ceeb86d8fb4a4
Tomas Zaluckij <me@tomaszal.com>
2024-08-30 19:28:39 +0300

    Remove unnecessary module type declarations

--- packages/config-eslint/plugins.d.ts
+++ packages/config-eslint/plugins.d.ts
@@ -1,8 +1,3 @@
-declare module 'eslint-plugin-jsx-a11y' {
-  import { TSESLint } from '@typescript-eslint/utils';
-  export const flatConfigs: { recommended: TSESLint.FlatConfig.Config };
-}
-
 declare module 'eslint-plugin-react/configs/recommended.js' {
   import { TSESLint } from '@typescript-eslint/utils';
   declare const config: TSESLint.FlatConfig.Config;
@@ -19,9 +14,4 @@ declare module 'eslint-plugin-react-hooks' {
   import { Linter, Rule } from 'eslint';
   export const configs: { recommended: { rules: Linter.RulesRecord } };
   export const rules: Record<string, Rule.OldStyleRule>;
-}
-
-declare module 'eslint-plugin-tailwindcss' {
-  import { TSESLint } from '@typescript-eslint/utils';
-  export const configs: { 'flat/recommended': TSESLint.FlatConfig.ConfigArray };
 }
