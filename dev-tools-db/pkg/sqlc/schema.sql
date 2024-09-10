6a4a5336054c027bbe7127bb60d72e66ca8bc006
ElecTwix <root@electwix.dev>
2024-09-10 11:03:04 +0300

    added header, query and formBody tables checks added for ulid 16 byte limit

--- dev-tools-db/pkg/sqlc/schema.sql
+++ dev-tools-db/pkg/sqlc/schema.sql
@@ -6,99 +6,18 @@ CREATE TABLE users (
   provider_type INT8 NOT NULL DEFAULT 0,
   provider_id TEXT,
   status INT8 NOT NULL DEFAULT 0,
+  CHECK (length(id) == 16),
   CHECK (status IN (0, 1, 2, 3)),
   CHECK (provider_type IN (0, 1, 2, 3)),
   UNIQUE (provider_type, provider_id)
 );
 
--- COLLECTIONS
-CREATE TABLE collections (
-  id BLOB NOT NULL PRIMARY KEY,
-  owner_id BLOB NOT NULL,
-  name TEXT NOT NULL,
-  FOREIGN KEY (owner_id) REFERENCES workspaces (id) ON DELETE CASCADE
-);
-
-CREATE INDEX Idx1 ON collections (owner_id);
-
-/*
-*
-* ITEM FOLDER
-*
-*/
--- ITEM FOLDER BASE TABLE
-CREATE TABLE item_folder (
-  id BLOB NOT NULL PRIMARY KEY,
-  collection_id BLOB NOT NULL,
-  parent_id BLOB,
-  name TEXT NOT NULL,
-  prev BLOB,
-  next BLOB,
-  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE,
-  FOREIGN KEY (parent_id) REFERENCES item_folder (id) ON DELETE CASCADE
-);
-
-CREATE INDEX Idx3 ON item_folder (collection_id, parent_id);
-
-/*
-* 
-* ITEM API
-*
-*/
-
--- ITEM API
-CREATE TABLE item_api (
-  id BLOB NOT NULL PRIMARY KEY,
-  collection_id BLOB NOT NULL,
-  parent_id BLOB,
-  name TEXT NOT NULL,
-  url TEXT NOT NULL,
-  method TEXT NOT NULL,
-  prev BLOB,
-  next BLOB,
-  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE,
-  FOREIGN KEY (parent_id) REFERENCES item_folder (id) ON DELETE CASCADE
-);
-
-CREATE INDEX Idx2 ON item_api (collection_id, parent_id);
-
-
-/*
-*
-* ITEM API EXAMPLE
-*
-*/
-CREATE TABLE item_api_example (
-  id BLOB NOT NULL PRIMARY KEY,
-  item_api_id BLOB NOT NULL,
-  collection_id BLOB NOT NULL,
-  parent_example_id BLOB,
-  is_default BOOLEAN NOT NULL DEFAULT FALSE,
-  name TEXT NOT NULL,
-  headers BLOB NOT NULL,
-  query BLOB NOT NULL,
-  compressed BOOLEAN NOT NULL DEFAULT FALSE,
-  body BLOB NOT NULL,
-  prev BLOB,
-  next BLOB,
-  FOREIGN KEY (parent_example_id) REFERENCES item_api_example (id) ON DELETE CASCADE,
-  FOREIGN KEY (item_api_id) REFERENCES item_api (id) ON DELETE CASCADE,
-  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE
-);
-
-CREATE INDEX item_api_example_idx1 ON item_api_example (
-  item_api_id,
-  collection_id,
-  is_default,
-  parent_example_id
-);
-
-
 -- WORK SPACES 
 CREATE TABLE workspaces (
   id BLOB NOT NULL PRIMARY KEY,
   name TEXT NOT NULL,
-  updated TIMESTAMP NOT NULL DEFAULT (unixepoch ())
+  updated TIMESTAMP NOT NULL DEFAULT (unixepoch ()),
+  CHECK (length(id) == 16)
 );
 
 -- WORKSPACE USERS
@@ -107,6 +26,7 @@ CREATE TABLE workspaces_users (
   workspace_id BLOB NOT NULL,
   user_id BLOB NOT NULL,
   role INT8 NOT NULL DEFAULT 1,
+  CHECK (length (id) == 16),
   CHECK (role IN (1, 2, 3)),
   UNIQUE (workspace_id, user_id),
   FOREIGN KEY (workspace_id) REFERENCES workspaces (id) ON DELETE CASCADE,
@@ -127,3 +47,137 @@ CREATE TABLE result_api (
 
 CREATE INDEX Idx4 ON result_api (trigger_by, trigger_type);
 
+-- COLLECTIONS
+CREATE TABLE collections (
+  id BLOB NOT NULL PRIMARY KEY,
+  owner_id BLOB NOT NULL,
+  name TEXT NOT NULL,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (owner_id) REFERENCES workspaces (id) ON DELETE CASCADE
+);
+
+CREATE INDEX Idx1 ON collections (owner_id);
+
+/*
+ *
+ * ITEM FOLDER
+ *
+ */
+-- ITEM FOLDER BASE TABLE
+CREATE TABLE item_folder (
+  id BLOB NOT NULL PRIMARY KEY,
+  collection_id BLOB NOT NULL,
+  parent_id BLOB,
+  name TEXT NOT NULL,
+  prev BLOB,
+  next BLOB,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE,
+  FOREIGN KEY (parent_id) REFERENCES item_folder (id) ON DELETE CASCADE
+);
+
+CREATE INDEX Idx3 ON item_folder (collection_id, parent_id);
+
+/*
+ * 
+ * ITEM API
+ *
+ */
+-- ITEM API
+CREATE TABLE item_api (
+  id BLOB NOT NULL PRIMARY KEY,
+  collection_id BLOB NOT NULL,
+  parent_id BLOB,
+  name TEXT NOT NULL,
+  url TEXT NOT NULL,
+  method TEXT NOT NULL,
+  prev BLOB,
+  next BLOB,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE,
+  FOREIGN KEY (parent_id) REFERENCES item_folder (id) ON DELETE CASCADE
+);
+
+CREATE INDEX Idx2 ON item_api (collection_id, parent_id);
+
+/*
+ *
+ * ITEM API EXAMPLE
+ *
+ */
+CREATE TABLE item_api_example (
+  id BLOB NOT NULL PRIMARY KEY,
+  item_api_id BLOB NOT NULL,
+  collection_id BLOB NOT NULL,
+  parent_example_id BLOB,
+  is_default BOOLEAN NOT NULL DEFAULT FALSE,
+  name TEXT NOT NULL,
+  headers BLOB NOT NULL,
+  query BLOB NOT NULL,
+  compressed BOOLEAN NOT NULL DEFAULT FALSE,
+  body BLOB NOT NULL,
+  prev BLOB,
+  next BLOB,
+  CHECK (length(prev) == 16),
+  CHECK (length(next) == 16),
+  CHECK (length(id) == 16),
+  UNIQUE (prev, next),
+  FOREIGN KEY (parent_example_id) REFERENCES item_api_example (id) ON DELETE CASCADE,
+  FOREIGN KEY (item_api_id) REFERENCES item_api (id) ON DELETE CASCADE,
+  FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE
+);
+
+CREATE INDEX item_api_example_idx1 ON item_api_example (
+  item_api_id,
+  collection_id,
+  is_default,
+  parent_example_id
+);
+
+CREATE TABLE item_api_example_header (
+  id BLOB NOT NULL PRIMARY KEY,
+  example_id BLOB NOT NULL,
+  header_key TEXT NOT NULL,
+  enable BOOLEAN NOT NULL DEFAULT TRUE,
+  description TEXT NOT NULL,
+  value TEXT NOT NULL,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (example_id) REFERENCES item_api_example (id) ON DELETE CASCADE
+);
+
+CREATE INDEX item_api_example_header_idx1 ON item_api_example_header (
+  example_id,
+  header_key
+);
+
+CREATE TABLE item_api_example_query (
+  id BLOB NOT NULL PRIMARY KEY,
+  example_id BLOB NOT NULL,
+  query_key TEXT NOT NULL,
+  enable BOOLEAN NOT NULL DEFAULT TRUE,
+  description TEXT NOT NULL,
+  value TEXT NOT NULL,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (example_id) REFERENCES item_api_example (id) ON DELETE CASCADE
+);
+
+CREATE INDEX item_api_example_query_idx1 ON item_api_example_query (
+  example_id,
+  query_key
+);
+
+CREATE TABLE item_api_example_body_form (
+  id BLOB NOT NULL PRIMARY KEY,
+  example_id BLOB NOT NULL,
+  body_key TEXT NOT NULL,
+  enable BOOLEAN NOT NULL DEFAULT TRUE,
+  description TEXT NOT NULL,
+  value TEXT NOT NULL,
+  CHECK (length (id) == 16),
+  FOREIGN KEY (example_id) REFERENCES item_api_example (id) ON DELETE CASCADE
+);
+
+CREATE INDEX item_api_example_body_form_idx1 ON item_api_example_body_form (
+  example_id,
+  body_key
+);
