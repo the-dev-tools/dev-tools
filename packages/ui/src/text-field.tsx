a55a6feebcea569776610a37866e86a6dee0e647
Tomas Zaluckij <me@tomaszal.com>
2024-09-23 14:44:35 +0100

    Implement text area components

--- packages/ui/src/text-field.tsx
+++ packages/ui/src/text-field.tsx
@@ -2,12 +2,15 @@ import { String, Struct } from 'effect';
 import { forwardRef } from 'react';
 import {
   Input as AriaInput,
+  TextArea as AriaTextArea,
   TextField as AriaTextField,
+  composeRenderProps,
   type InputProps as AriaInputProps,
+  type TextAreaProps as AriaTextAreaProps,
   type TextFieldProps as AriaTextFieldProps,
 } from 'react-aria-components';
 import { FieldPath, FieldValues, useController, UseControllerProps } from 'react-hook-form';
-import { tv, type VariantProps } from 'tailwind-variants';
+import { tv } from 'tailwind-variants';
 
 import { splitProps, type MixinProps } from '@the-dev-tools/utils/mixin-props';
 
@@ -17,69 +20,63 @@ import { controllerPropKeys, ControllerPropKeys } from './react-hook-form';
 import { tw } from './tailwind-literal';
 import { composeRenderPropsTV, composeRenderPropsTW } from './utils';
 
-// Root
-
-export interface TextFieldRootProps extends AriaTextFieldProps {}
-
-export const TextFieldRoot = ({ className, ...props }: TextFieldRootProps) => (
-  <AriaTextField {...props} className={composeRenderPropsTW(className, tw`flex flex-col gap-1`)} />
-);
-
 // Input
 
-export const textFieldInputStyles = tv({
+const inputStyles = tv({
   extend: focusRingStyles,
   base: tw`col-start-2 rounded border border-black px-2 py-1 rac-invalid:border-red-600`,
 });
 
-export interface TextFieldInputProps extends AriaInputProps, VariantProps<typeof textFieldInputStyles> {}
+// Root
 
-export const TextFieldInput = forwardRef(
-  ({ className, ...props }: TextFieldInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
-    const forwardedProps = Struct.omit(props, ...textFieldInputStyles.variantKeys);
-    const variantProps = Struct.pick(props, ...textFieldInputStyles.variantKeys);
-    return (
-      <AriaInput
-        {...forwardedProps}
-        ref={ref}
-        className={composeRenderPropsTV(className, textFieldInputStyles, variantProps)}
-      />
-    );
-  },
-);
-TextFieldInput.displayName = 'TextFieldInput';
-
-// Mix
-
-export interface TextFieldProps
-  extends Omit<TextFieldRootProps, 'children'>,
+interface RootProps
+  extends AriaTextFieldProps,
     MixinProps<'label', Omit<FieldLabelProps, 'children'>>,
-    MixinProps<'input', Omit<TextFieldInputProps, 'children'>>,
     MixinProps<'error', Omit<FieldErrorProps, 'children'>> {
   label?: FieldLabelProps['children'];
   error?: FieldErrorProps['children'];
 }
 
-export const TextField = forwardRef(
-  ({ label, error, ...props }: TextFieldProps, ref: React.ForwardedRef<HTMLInputElement>) => {
-    const forwardedProps = splitProps(props, 'label', 'input', 'error');
+const Root = ({ className, children, label, error, ...props }: RootProps) => {
+  const forwardedProps = splitProps(props, 'label', 'error');
 
-    if (!label && !forwardedProps.rest['aria-label'] && forwardedProps.rest.name) {
-      forwardedProps.rest['aria-label'] = String.capitalize(forwardedProps.rest.name);
-    }
+  if (!label && !forwardedProps.rest['aria-label'] && forwardedProps.rest.name) {
+    forwardedProps.rest['aria-label'] = String.capitalize(forwardedProps.rest.name);
+  }
+
+  return (
+    <AriaTextField {...forwardedProps.rest} className={composeRenderPropsTW(className, tw`flex flex-col gap-1`)}>
+      {composeRenderProps(children, (children) => (
+        <>
+          {label && <FieldLabel {...forwardedProps.label}>{label}</FieldLabel>}
+          {children}
+          <FieldError {...forwardedProps.error}>{error}</FieldError>
+        </>
+      ))}
+    </AriaTextField>
+  );
+};
+
+// Text field
+
+export interface TextFieldProps
+  extends Omit<RootProps, 'children'>,
+    MixinProps<'input', Omit<AriaInputProps, 'children'>> {}
+
+export const TextField = forwardRef(
+  ({ inputClassName, ...props }: TextFieldProps, ref: React.ForwardedRef<HTMLInputElement>) => {
+    const forwardedProps = splitProps(props, 'input');
 
     return (
-      <TextFieldRoot {...forwardedProps.rest}>
-        {label && <FieldLabel {...forwardedProps.label}>{label}</FieldLabel>}
-        <TextFieldInput {...forwardedProps.input} ref={ref} />
-        <FieldError {...forwardedProps.error}>{error}</FieldError>
-      </TextFieldRoot>
+      <Root {...forwardedProps.rest}>
+        <AriaInput {...forwardedProps} ref={ref} className={composeRenderPropsTV(inputClassName, inputStyles)} />
+      </Root>
     );
   },
 );
 TextField.displayName = 'TextField';
 
-// RHF wrapper mix
+// Text field RHF wrapper
 
 export interface TextFieldRHFProps<
   TFieldValues extends FieldValues = FieldValues,
@@ -106,9 +103,76 @@ export const TextFieldRHF = <
 ) => {
   const forwardedProps = Struct.omit(props, ...controllerPropKeys);
   const controllerProps = Struct.pick(props, ...controllerPropKeys);
+
   const { field, fieldState } = useController(controllerProps);
+
   return (
     <TextField
+      {...forwardedProps}
+      ref={field.ref}
+      name={field.name}
+      value={field.value}
+      onChange={field.onChange}
+      onBlur={field.onBlur}
+      isDisabled={field.disabled ?? false}
+      validationBehavior='aria'
+      isInvalid={fieldState.invalid}
+      error={fieldState.error?.message}
+    />
+  );
+};
+
+// Text area field
+
+export interface TextAreaFieldProps
+  extends Omit<RootProps, 'children'>,
+    MixinProps<'area', Omit<AriaTextAreaProps, 'children'>> {}
+
+export const TextAreaField = forwardRef(
+  ({ areaClassName, ...props }: TextAreaFieldProps, ref: React.ForwardedRef<HTMLTextAreaElement>) => {
+    const forwardedProps = splitProps(props, 'area');
+
+    return (
+      <Root {...forwardedProps.rest}>
+        <AriaTextArea {...forwardedProps.area} ref={ref} className={composeRenderPropsTV(areaClassName, inputStyles)} />
+      </Root>
+    );
+  },
+);
+TextAreaField.displayName = 'TextAreaField';
+
+// Text area field RHF wrapper
+
+export interface TextAreaFieldRHFProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> extends Omit<
+      TextAreaFieldProps,
+      | ControllerPropKeys
+      | 'name'
+      | 'value'
+      | 'onChange'
+      | 'onBlur'
+      | 'isDisabled'
+      | 'validationBehavior'
+      | 'isInvalid'
+      | 'error'
+    >,
+    UseControllerProps<TFieldValues, TName> {}
+
+export const TextAreaFieldRHF = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(
+  props: TextAreaFieldRHFProps<TFieldValues, TName>,
+) => {
+  const forwardedProps = Struct.omit(props, ...controllerPropKeys);
+  const controllerProps = Struct.pick(props, ...controllerPropKeys);
+
+  const { field, fieldState } = useController(controllerProps);
+
+  return (
+    <TextAreaField
       {...forwardedProps}
       ref={field.ref}
       name={field.name}
