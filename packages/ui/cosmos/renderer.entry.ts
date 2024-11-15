12d592252f3e9aae82d48ade85aaa614f6d3ae29
Tomas Zaluckij <me@tomaszal.com>
2024-11-15 17:00:07 +0000

    Remove unused lint ignore rules

--- packages/ui/cosmos/renderer.entry.ts
+++ packages/ui/cosmos/renderer.entry.ts
@@ -1,10 +1,8 @@
 import { mountDomRenderer } from 'react-cosmos-dom';
 
-// eslint-disable-next-line import-x/no-unresolved
 import * as mountArgs from './imports';
 
 import '../src/fonts';
 import './styles.css';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 mountDomRenderer(mountArgs);
