7424f5a60770bcdaa41788b9f452997ab2d0ca27
ElecTwix <root@electwix.dev>
2025-01-23 22:33:30 +0300

    update(flowlocalrunner): update parallel execution
    - Implemented iterative approach to handle parallel execution of nodes
    - Ensured nodes are executed in parallel and merged correctly
    - Added test case for split and merge with subnodes
    - Updated RunNodeSync and RunNodeASync functions to avoid recursion
    - Ensured merged nodes are executed only once after parallel nodes finish

--- apps/backend/pkg/flow/runner/flowlocalrunner/flowlocalrunner.go
+++ apps/backend/pkg/flow/runner/flowlocalrunner/flowlocalrunner.go
@@ -39,14 +39,13 @@ func (r FlowLocalRunner) Run(ctx context.Context, status chan runner.FlowStatusR
 		status <- runner.NewFlowStatus(runner.FlowStatusRunning, node.NodeStatusRunning, &id)
 	}
 
-	var readerWriterLock sync.RWMutex
 	req := &node.FlowNodeRequest{
 		VarMap:        map[string]interface{}{},
+		ReadWriteLock: &sync.RWMutex{},
 		NodeMap:       r.FlowNodeMap,
 		EdgeSourceMap: r.EdgesMap,
 		LogPushFunc:   node.LogPushFunc(logWorkaround),
 		Timeout:       r.Timeout,
-		ReadWriteLock: &readerWriterLock,
 	}
 	fmt.Println("FlowLocalRunner.Run")
 	status <- runner.NewFlowStatus(runner.FlowStatusStarting, node.NodeNone, nil)
@@ -70,114 +69,176 @@ func (r FlowLocalRunner) Run(ctx context.Context, status chan runner.FlowStatusR
 	return nil
 }
 
-func RunNodeSync(ctx context.Context, currentNode node.FlowNode, req *node.FlowNodeRequest, statusLogFunc node.LogPushFunc) ([]idwrap.IDWrap, error) {
-	id := currentNode.GetID()
-	statusLogFunc(node.NodeStatusRunning, id)
-	res := currentNode.RunSync(ctx, req)
-
-	nextNodeLen := len(res.NextNodeID)
-	if nextNodeLen == 0 {
-		return nil, res.Err
-	}
-	if nextNodeLen == 1 {
-		// TODO: check for hashmap before getting the next node
-		return RunNodeSync(ctx, req.NodeMap[res.NextNodeID[0]], req, statusLogFunc)
-	}
-	var wg sync.WaitGroup
-
-	parallel := make(chan node.FlowNodeResult, nextNodeLen)
-	for _, v := range res.NextNodeID {
-		currentNode, ok := req.NodeMap[v]
-		if !ok {
-			return nil, fmt.Errorf("node not found: %v", v)
-		}
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			res, err := RunNodeSync(ctx, currentNode, req, statusLogFunc)
-			parallel <- node.FlowNodeResult{
-				NextNodeID: res,
-				Err:        err,
-			}
-		}()
-	}
-
-	wg.Wait()
-
+func RunNodeSync(ctx context.Context, startNode node.FlowNode, req *node.FlowNodeRequest, statusLogFunc node.LogPushFunc) ([]idwrap.IDWrap, error) {
 	var nextNodeID []idwrap.IDWrap
-	for i := 0; i < nextNodeLen; i++ {
-		a := <-parallel
-		if a.Err != nil {
-			return nil, a.Err
+	queue := []node.FlowNode{startNode}
+	processedNodes := make(map[idwrap.IDWrap]bool)
+
+	for len(queue) > 0 {
+		currentNode := queue[0]
+		queue = queue[1:]
+
+		id := currentNode.GetID()
+		if processedNodes[id] {
+			continue
 		}
-		nextNodeID = append(nextNodeID, a.NextNodeID...)
+		processedNodes[id] = true
+
+		statusLogFunc(node.NodeStatusRunning, id)
+		res := currentNode.RunSync(ctx, req)
+
+		nextNodeLen := len(res.NextNodeID)
+		if nextNodeLen == 0 {
+			if res.Err != nil {
+				return nil, res.Err
+			}
+			continue
+		}
+
+		if nextNodeLen == 1 {
+			nextNode, ok := req.NodeMap[res.NextNodeID[0]]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", res.NextNodeID[0])
+			}
+			queue = append(queue, nextNode)
+			continue
+		}
+
+		var wg sync.WaitGroup
+		parallel := make(chan node.FlowNodeResult, nextNodeLen)
+		for _, v := range res.NextNodeID {
+			nextNode, ok := req.NodeMap[v]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", v)
+			}
+			wg.Add(1)
+			go func(nextNode node.FlowNode) {
+				defer wg.Done()
+				res := nextNode.RunSync(ctx, req)
+				parallel <- node.FlowNodeResult{
+					NextNodeID: res.NextNodeID,
+					Err:        res.Err,
+				}
+			}(nextNode)
+		}
+
+		wg.Wait()
+		close(parallel)
+
+		mergedNextNodeIDs := make(map[idwrap.IDWrap]bool)
+		for a := range parallel {
+			if a.Err != nil {
+				return nil, a.Err
+			}
+			for _, nextID := range a.NextNodeID {
+				mergedNextNodeIDs[nextID] = true
+			}
+		}
+
+		for nextID := range mergedNextNodeIDs {
+			nextNode, ok := req.NodeMap[nextID]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", nextID)
+			}
+			queue = append(queue, nextNode)
+		}
 	}
 
-	return nextNodeID, res.Err
+	return nextNodeID, nil
 }
 
-func RunNodeASync(ctx context.Context, currentNode node.FlowNode, req *node.FlowNodeRequest, statusLogFunc node.LogPushFunc) ([]idwrap.IDWrap, error) {
-	id := currentNode.GetID()
-	statusLogFunc(node.NodeStatusRunning, id)
-
-	resChan := make(chan node.FlowNodeResult, 1)
-	go func() {
-		res := currentNode.RunSync(ctx, req)
-		resChan <- res
-	}()
-
-	var res *node.FlowNodeResult
-	timedCtx, cancel := context.WithTimeout(ctx, req.Timeout)
-	defer cancel()
-
-	select {
-	case <-ctx.Done():
-		return nil, fmt.Errorf("context done")
-	case resLocal := <-resChan:
-		res = &resLocal
-	case <-timedCtx.Done():
-		return nil, fmt.Errorf("timeout")
-	}
-
-	nextNodeLen := len(res.NextNodeID)
-	if nextNodeLen == 0 {
-		return nil, res.Err
-	}
-	if nextNodeLen == 1 {
-		return RunNodeASync(ctx, req.NodeMap[res.NextNodeID[0]], req, statusLogFunc)
-	}
-
-	var wg sync.WaitGroup
-	parallel := make(chan node.FlowNodeResult, nextNodeLen)
-	for _, v := range res.NextNodeID {
-		currentNode, ok := req.NodeMap[v]
-		if !ok {
-			return nil, fmt.Errorf("node not found: %v", v)
-		}
-		wg.Add(1)
-		go func(currentNode node.FlowNode) {
-			defer wg.Done()
-			_, err := RunNodeASync(ctx, currentNode, req, statusLogFunc)
-			if err != nil {
-				parallel <- node.FlowNodeResult{
-					NextNodeID: nil,
-					Err:        err,
-				}
-				return
-			}
-		}(currentNode)
-	}
-
-	wg.Wait()
-	close(parallel)
-
+func RunNodeASync(ctx context.Context, startNode node.FlowNode, req *node.FlowNodeRequest, statusLogFunc node.LogPushFunc) ([]idwrap.IDWrap, error) {
 	var nextNodeID []idwrap.IDWrap
-	for a := range parallel {
-		if a.Err != nil {
-			return nil, a.Err
+	queue := []node.FlowNode{startNode}
+	processedNodes := make(map[idwrap.IDWrap]bool)
+
+	for len(queue) > 0 {
+		currentNode := queue[0]
+		queue = queue[1:]
+
+		id := currentNode.GetID()
+		if processedNodes[id] {
+			continue
 		}
-		nextNodeID = append(nextNodeID, a.NextNodeID...)
+		processedNodes[id] = true
+
+		statusLogFunc(node.NodeStatusRunning, id)
+
+		resChan := make(chan node.FlowNodeResult, 1)
+		go func() {
+			res := currentNode.RunSync(ctx, req)
+			resChan <- res
+		}()
+
+		var res *node.FlowNodeResult
+		timedCtx, cancel := context.WithTimeout(ctx, req.Timeout)
+		defer cancel()
+
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("context done")
+		case resLocal := <-resChan:
+			res = &resLocal
+		case <-timedCtx.Done():
+			return nil, fmt.Errorf("timeout")
+		}
+
+		nextNodeLen := len(res.NextNodeID)
+		if nextNodeLen == 0 {
+			if res.Err != nil {
+				return nil, res.Err
+			}
+			continue
+		}
+
+		if nextNodeLen == 1 {
+			nextNode, ok := req.NodeMap[res.NextNodeID[0]]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", res.NextNodeID[0])
+			}
+			queue = append(queue, nextNode)
+			continue
+		}
+
+		var wg sync.WaitGroup
+		parallel := make(chan node.FlowNodeResult, nextNodeLen)
+		for _, v := range res.NextNodeID {
+			nextNode, ok := req.NodeMap[v]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", v)
+			}
+			wg.Add(1)
+			go func(nextNode node.FlowNode) {
+				defer wg.Done()
+				res := nextNode.RunSync(ctx, req)
+				parallel <- node.FlowNodeResult{
+					NextNodeID: res.NextNodeID,
+					Err:        res.Err,
+				}
+			}(nextNode)
+		}
+
+		wg.Wait()
+		close(parallel)
+
+		mergedNextNodeIDs := make(map[idwrap.IDWrap]bool)
+		for a := range parallel {
+			if a.Err != nil {
+				return nil, a.Err
+			}
+			for _, nextID := range a.NextNodeID {
+				mergedNextNodeIDs[nextID] = true
+			}
+		}
+
+		for nextID := range mergedNextNodeIDs {
+			nextNode, ok := req.NodeMap[nextID]
+			if !ok {
+				return nil, fmt.Errorf("node not found: %v", nextID)
+			}
+			queue = append(queue, nextNode)
+		}
 	}
 
-	return nextNodeID, res.Err
+	return nextNodeID, nil
 }
--- apps/backend/pkg/flow/runner/flowlocalrunner/flowlocalrunner_test.go
+++ apps/backend/pkg/flow/runner/flowlocalrunner/flowlocalrunner_test.go
@@ -201,3 +201,113 @@ func TestLocalFlowRunner_Run_ParallelExecution(t *testing.T) {
 		t.Errorf("Expected node4 to start after node2 and node3 ended, but got %v, %v and %v", endTimes[node4ID], endTimes[node2ID], endTimes[node3ID])
 	}
 }
+
+func TestLocalFlowRunner_Run_SplitAndMerge(t *testing.T) {
+	var runCounter atomic.Int32
+	onRun := func() {
+		runCounter.Add(1)
+	}
+
+	const CountShouldRun = 4
+
+	node1ID := idwrap.NewNow()
+	node2ID := idwrap.NewNow()
+	node3ID := idwrap.NewNow()
+	node4ID := idwrap.NewNow()
+
+	mockNode1 := mocknode.NewMockNode(node1ID, []idwrap.IDWrap{node2ID, node3ID}, onRun)
+	mockNode2 := mocknode.NewMockNode(node2ID, []idwrap.IDWrap{node4ID}, onRun)
+	mockNode3 := mocknode.NewMockNode(node3ID, []idwrap.IDWrap{node4ID}, onRun)
+	mockNode4 := mocknode.NewMockNode(node4ID, nil, onRun)
+
+	flowNodeMap := map[idwrap.IDWrap]node.FlowNode{
+		node1ID: mockNode1,
+		node2ID: mockNode2,
+		node3ID: mockNode3,
+		node4ID: mockNode4,
+	}
+
+	edge1 := edge.NewEdge(idwrap.NewNow(), node1ID, node2ID, edge.HandleUnspecified)
+	edge2 := edge.NewEdge(idwrap.NewNow(), node1ID, node3ID, edge.HandleUnspecified)
+	edge3 := edge.NewEdge(idwrap.NewNow(), node2ID, node4ID, edge.HandleUnspecified)
+	edge4 := edge.NewEdge(idwrap.NewNow(), node3ID, node4ID, edge.HandleUnspecified)
+	edges := []edge.Edge{edge1, edge2, edge3, edge4}
+	edgesMap := edge.NewEdgesMap(edges)
+
+	runnerLocal := flowlocalrunner.CreateFlowRunner(idwrap.NewNow(), idwrap.NewNow(), node1ID, flowNodeMap, edgesMap, time.Minute)
+	statusChan := make(chan runner.FlowStatusResp, 10)
+	err := runnerLocal.Run(context.Background(), statusChan)
+	if err != nil {
+		t.Errorf("Expected err to be nil, but got %v", err)
+	}
+	a := runCounter.Load()
+	if a != CountShouldRun {
+		t.Errorf("Expected runCounter to be %d, but got %d", CountShouldRun, a)
+	}
+}
+
+func TestLocalFlowRunner_Run_SplitAndMergeWithSubNodes(t *testing.T) {
+	var runCounter atomic.Int32
+	onRun := func() {
+		runCounter.Add(1)
+	}
+
+	const CountShouldRun = 9
+
+	node1ID := idwrap.NewNow()
+	node2ID := idwrap.NewNow()
+	node3ID := idwrap.NewNow()
+	node4ID := idwrap.NewNow()
+	node5ID := idwrap.NewNow()
+	node6ID := idwrap.NewNow()
+	node7ID := idwrap.NewNow()
+	node8ID := idwrap.NewNow()
+	node9ID := idwrap.NewNow()
+
+	mockNode1 := mocknode.NewMockNode(node1ID, []idwrap.IDWrap{node2ID, node3ID}, onRun)
+	mockNode2 := mocknode.NewMockNode(node2ID, []idwrap.IDWrap{node4ID, node5ID}, onRun)
+	mockNode3 := mocknode.NewMockNode(node3ID, []idwrap.IDWrap{node6ID, node7ID}, onRun)
+	mockNode4 := mocknode.NewMockNode(node4ID, []idwrap.IDWrap{node8ID}, onRun)
+	mockNode5 := mocknode.NewMockNode(node5ID, []idwrap.IDWrap{node8ID}, onRun)
+	mockNode6 := mocknode.NewMockNode(node6ID, []idwrap.IDWrap{node9ID}, onRun)
+	mockNode7 := mocknode.NewMockNode(node7ID, []idwrap.IDWrap{node9ID}, onRun)
+	mockNode8 := mocknode.NewMockNode(node8ID, []idwrap.IDWrap{node9ID}, onRun)
+	mockNode9 := mocknode.NewMockNode(node9ID, nil, onRun)
+
+	flowNodeMap := map[idwrap.IDWrap]node.FlowNode{
+		node1ID: mockNode1,
+		node2ID: mockNode2,
+		node3ID: mockNode3,
+		node4ID: mockNode4,
+		node5ID: mockNode5,
+		node6ID: mockNode6,
+		node7ID: mockNode7,
+		node8ID: mockNode8,
+		node9ID: mockNode9,
+	}
+
+	edge1 := edge.NewEdge(idwrap.NewNow(), node1ID, node2ID, edge.HandleUnspecified)
+	edge2 := edge.NewEdge(idwrap.NewNow(), node1ID, node3ID, edge.HandleUnspecified)
+	edge3 := edge.NewEdge(idwrap.NewNow(), node2ID, node4ID, edge.HandleUnspecified)
+	edge4 := edge.NewEdge(idwrap.NewNow(), node2ID, node5ID, edge.HandleUnspecified)
+	edge5 := edge.NewEdge(idwrap.NewNow(), node3ID, node6ID, edge.HandleUnspecified)
+	edge6 := edge.NewEdge(idwrap.NewNow(), node3ID, node7ID, edge.HandleUnspecified)
+	edge7 := edge.NewEdge(idwrap.NewNow(), node4ID, node8ID, edge.HandleUnspecified)
+	edge8 := edge.NewEdge(idwrap.NewNow(), node5ID, node8ID, edge.HandleUnspecified)
+	edge9 := edge.NewEdge(idwrap.NewNow(), node6ID, node9ID, edge.HandleUnspecified)
+	edge10 := edge.NewEdge(idwrap.NewNow(), node7ID, node9ID, edge.HandleUnspecified)
+	edge11 := edge.NewEdge(idwrap.NewNow(), node8ID, node9ID, edge.HandleUnspecified)
+	edges := []edge.Edge{edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11}
+	edgesMap := edge.NewEdgesMap(edges)
+
+	runnerLocal := flowlocalrunner.CreateFlowRunner(idwrap.NewNow(), idwrap.NewNow(), node1ID, flowNodeMap, edgesMap, time.Minute)
+	statusChan := make(chan runner.FlowStatusResp, 10)
+	err := runnerLocal.Run(context.Background(), statusChan)
+	if err != nil {
+		t.Errorf("Expected err to be nil, but got %v", err)
+	}
+	a := runCounter.Load()
+	if a != CountShouldRun {
+		t.Errorf("Expected runCounter to be %d, but got %d", CountShouldRun, a)
+	}
+}
