ce7ba0a63e2aebe28185060fd6d59568224f67f5
Tomas Zaluckij <me@tomaszal.com>
2025-02-02 03:50:08 +0000

    Fix project info evaluation in workflows

--- libraries/scripts/src/project-info-output.ts
+++ libraries/scripts/src/project-info-output.ts
@@ -1,9 +1,5 @@
 import { execSync } from 'node:child_process';
-import { writeFileSync } from 'node:fs';
-
-import { goToRoot } from './go-to-root.ts';
-
-goToRoot();
+import { appendFileSync } from 'node:fs';
 
 const tag = process.env['GITHUB_REF_TYPE'] === 'tag' ? process.env['GITHUB_REF_NAME'] : undefined;
 const [name, version] = tag?.split('@') ?? [];
@@ -13,18 +9,14 @@ const infoJson = execSync(`nx show project ${name} --json`, { encoding: 'utf8' }
 const info: unknown = JSON.parse(infoJson);
 if (!info) throw new Error('Could not determine project information');
 
-const output = Object.entries({ ...info, version })
-  .map(([key, value]) => {
-    if (typeof value !== 'string') return '';
+Object.entries({ ...info, version }).forEach(([key, value]) => {
+  if (typeof value !== 'string') return;
 
-    // $exampleKey -> EXAMPLE_KEY
-    const varKey = key
-      .replace(/([A-Z])/g, '_$1')
-      .toUpperCase()
-      .replace(/[^A-Z_]/g, '');
+  // $exampleKey -> EXAMPLE_KEY
+  const varKey = key
+    .replace(/([A-Z])/g, '_$1')
+    .toUpperCase()
+    .replace(/[^A-Z_]/g, '');
 
-    return `echo "${varKey}=${value}" >> "$GITHUB_OUTPUT"`;
-  })
-  .join('\n');
-
-writeFileSync('project-info-output', output);
+  appendFileSync(process.env['GITHUB_OUTPUT'] ?? '', `${varKey}=${value}\n`);
+});
