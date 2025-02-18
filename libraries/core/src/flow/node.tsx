fc6fd1347c6c59a038d22ac72ea9de270602e72c
Tomas Zaluckij <me@tomaszal.com>
2025-02-18 21:21:50 +0000

    Disable selection for no op nodes

--- libraries/core/src/flow/node.tsx
+++ libraries/core/src/flow/node.tsx
@@ -64,6 +64,7 @@ export const Node = {
     position: Struct.pick(position!, 'x', 'y'),
     origin: [0.5, 0],
     type: enumToJson(NodeKindSchema, kind),
+    selectable: ![NodeKind.UNSPECIFIED, NodeKind.NO_OP].includes(kind),
     data: { ...Struct.omit(data, '$typeName', '$unknown'), state: NodeState.UNSPECIFIED },
   }),
 
