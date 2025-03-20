47398f2313e3a0e11d9ac506a474f79ba1327354
Tomas Zaluckij <me@tomaszal.com>
2025-03-20 17:30:27 +0000

    Move email templates into private repository

--- libraries/email/src/utils.tsx
+++
@@ -1,8 +0,0 @@
-import { Record } from 'effect';
-
-export const makeGoVariable = <T extends string>(id: T) => `{{ .${id} }}` as const;
-
-export const makeGoVariables = <T extends string[]>(...ids: T) =>
-  Record.fromIterableWith(ids, (id) => [id, makeGoVariable(id)] as const) as {
-    [Key in T[number]]: ReturnType<typeof makeGoVariable<Key>>;
-  };
