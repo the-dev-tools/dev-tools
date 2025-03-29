ff8363a9e5aef18d32ab6a80411358594d30342d
ElecTwix <root@electwix.dev>
2025-03-29 21:28:26 +0300

    feat(cli/flow): add flow running

--- apps/cli/cmd/flow.go
+++ apps/cli/cmd/flow.go
@@ -1,15 +1,46 @@
 package cmd
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"math"
 	"os"
+	"strings"
+	"the-dev-tools/backend/pkg/compress"
+	"the-dev-tools/backend/pkg/flow/edge"
+	"the-dev-tools/backend/pkg/flow/node"
+	"the-dev-tools/backend/pkg/flow/node/nfor"
+	"the-dev-tools/backend/pkg/flow/node/nforeach"
+	"the-dev-tools/backend/pkg/flow/node/nif"
+	"the-dev-tools/backend/pkg/flow/node/njs"
+	"the-dev-tools/backend/pkg/flow/node/nnoop"
+	"the-dev-tools/backend/pkg/flow/node/nrequest"
+	"the-dev-tools/backend/pkg/flow/runner"
+	"the-dev-tools/backend/pkg/flow/runner/flowlocalrunner"
+	"the-dev-tools/backend/pkg/http/request"
+	"the-dev-tools/backend/pkg/httpclient"
+	"the-dev-tools/backend/pkg/idwrap"
 	"the-dev-tools/backend/pkg/ioworkspace"
+	"the-dev-tools/backend/pkg/logconsole"
+	"the-dev-tools/backend/pkg/model/mexampleresp"
+	"the-dev-tools/backend/pkg/model/mflow"
+	"the-dev-tools/backend/pkg/model/mnnode"
+	"the-dev-tools/backend/pkg/model/mnnode/mnfor"
+	"the-dev-tools/backend/pkg/model/mnnode/mnforeach"
+	"the-dev-tools/backend/pkg/model/mnnode/mnif"
+	"the-dev-tools/backend/pkg/model/mnnode/mnjs"
+	"the-dev-tools/backend/pkg/model/mnnode/mnnoop"
+	"the-dev-tools/backend/pkg/model/mnnode/mnrequest"
+	"the-dev-tools/backend/pkg/reference"
 	"the-dev-tools/backend/pkg/service/sassert"
 	"the-dev-tools/backend/pkg/service/sassertres"
 	"the-dev-tools/backend/pkg/service/sbodyform"
 	"the-dev-tools/backend/pkg/service/sbodyraw"
 	"the-dev-tools/backend/pkg/service/sbodyurl"
 	"the-dev-tools/backend/pkg/service/scollection"
+	"the-dev-tools/backend/pkg/service/sedge"
 	"the-dev-tools/backend/pkg/service/sexampleheader"
 	"the-dev-tools/backend/pkg/service/sexamplequery"
 	"the-dev-tools/backend/pkg/service/sexampleresp"
@@ -28,9 +59,49 @@ import (
 	"the-dev-tools/backend/pkg/service/sworkspace"
 	"the-dev-tools/db/pkg/sqlc/gen"
 	"the-dev-tools/db/pkg/tursomem"
+	"the-dev-tools/spec/dist/buf/go/nodejs_executor/v1/nodejs_executorv1connect"
+	"time"
 
+	"connectrpc.com/connect"
 	"github.com/spf13/cobra"
 )
+
+type FLowServiceLocal struct {
+	DB *sql.DB
+	ws sworkspace.WorkspaceService
+
+	// flow
+	fs  sflow.FlowService
+	fes sedge.EdgeService
+
+	// request
+	ias sitemapi.ItemApiService
+	es  sitemapiexample.ItemApiExampleService
+	qs  sexamplequery.ExampleQueryService
+	hs  sexampleheader.HeaderService
+
+	// body
+	brs  sbodyraw.BodyRawService
+	bfs  sbodyform.BodyFormService
+	bues sbodyurl.BodyURLEncodedService
+
+	// response
+	ers  sexampleresp.ExampleRespService
+	erhs sexamplerespheader.ExampleRespHeaderService
+	as   sassert.AssertService
+	ars  sassertres.AssertResultService
+
+	// sub nodes
+	ns   snode.NodeService
+	rns  snoderequest.NodeRequestService
+	fns  snodefor.NodeForService
+	fens snodeforeach.NodeForEachService
+	sns  snodenoop.NodeNoopService
+	ins  snodeif.NodeIfService
+	jsns snodejs.NodeJSService
+
+	logChanMap logconsole.LogChanMap
+}
 
 func init() {
 	rootCmd.AddCommand(flowCmd)
@@ -87,6 +158,7 @@ var flowCmd = &cobra.Command{
 		flowForService := snodefor.New(queries)
 		flowForEachService := snodeforeach.New(queries)
 		flowJSService := snodejs.New(queries)
+		flowEdges := sedge.New(queries)
 
 		ioWorkspaceService := ioworkspace.NewIOWorkspaceService(
 			db,
@@ -114,6 +186,39 @@ var flowCmd = &cobra.Command{
 			flowJSService,
 		)
 
+		logMap := logconsole.NewLogChanMap()
+
+		flowServiceLocal := FLowServiceLocal{
+			DB:         db,
+			ws:         workspaceService,
+			fs:         flowService,
+			fes:        flowEdges,
+			ias:        endpointService,
+			es:         exampleService,
+			qs:         exampleQueryService,
+			hs:         exampleHeaderService,
+			brs:        rawBodyService,
+			bfs:        formBodyService,
+			bues:       urlBodyService,
+			ers:        responseService,
+			erhs:       responseHeaderService,
+			as:         exampleAssertService,
+			ars:        responseAssertService,
+			ns:         flowNodeService,
+			rns:        flowRequestService,
+			fns:        flowForService,
+			fens:       flowForEachService,
+			sns:        flowNoopService,
+			ins:        *flowConditionService,
+			jsns:       flowJSService,
+			logChanMap: logMap,
+		}
+
+		// TODO: move to const
+		ctxValue := context.WithValue(ctx, "flowServiceLocal", flowServiceLocal)
+		ctxValue = context.WithValue(ctx, "workspaceID", workspaceData.Workspace.ID)
+		cmd.SetContext(ctxValue)
+
 		err = ioWorkspaceService.ImportWorkspace(ctx, *workspaceData)
 		if err != nil {
 			return err
@@ -126,7 +231,397 @@ var flowRunCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Print the version number of DevToolsCLI",
 	Long:  `All software has versions. This is DevToolsCLI's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("DevToolsCLI %s\n", version)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		// TODO: move to const
+		flowLocalDataInterface := ctx.Value("flowServiceLocal")
+		flowLocalData := flowLocalDataInterface.(FLowServiceLocal)
+		c := flowLocalData
+
+		workspaceIDInterface := ctx.Value("workspaceID")
+		workspaceID := workspaceIDInterface.(idwrap.IDWrap)
+
+		if len(args) < 1 {
+			return errors.New("args should be more then 0")
+		}
+
+		nameOrID := args[0]
+
+		var flowPtr *mflow.Flow
+		// check if id
+		id, err := idwrap.NewText(nameOrID)
+		if err != nil {
+			// seems like not id try find
+
+			flows, err := c.fs.GetFlowsByWorkspaceID(ctx, workspaceID)
+			if err != nil {
+				return err
+			}
+			const matchComparRate = 2
+			for _, flow := range flows {
+				if math.Abs(float64(strings.Compare(nameOrID, flow.Name))) < matchComparRate {
+					flowPtr = &flow
+				}
+			}
+
+		} else {
+			flow, err := c.fs.GetFlow(ctx, id)
+			if err != nil {
+				return err
+			}
+			flowPtr = &flow
+		}
+
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		return flowRun(ctx, flowPtr, c)
 	},
+}
+
+func flowRun(ctx context.Context, flowPtr *mflow.Flow, c FLowServiceLocal) error {
+	latestFlowID := flowPtr.ID
+
+	nodes, err := c.ns.GetNodesByFlowID(ctx, latestFlowID)
+	if err != nil {
+		return connect.NewError(connect.CodeInternal, errors.New("get nodes"))
+	}
+
+	edges, err := c.fes.GetEdgesByFlowID(ctx, latestFlowID)
+	if err != nil {
+		return connect.NewError(connect.CodeInternal, errors.New("get edges"))
+	}
+	edgeMap := edge.NewEdgesMap(edges)
+
+	var requestNodes []mnrequest.MNRequest
+	var forNodes []mnfor.MNFor
+	var forEachNodes []mnforeach.MNForEach
+	var ifNodes []mnif.MNIF
+	var noopNodes []mnnoop.NoopNode
+	var jsNodes []mnjs.MNJS
+	var startNodeID idwrap.IDWrap
+
+	nodeNameMap := make(map[idwrap.IDWrap]string, len(nodes))
+
+	for _, node := range nodes {
+		nodeNameMap[node.ID] = node.Name
+
+		switch node.NodeKind {
+		case mnnode.NODE_KIND_REQUEST:
+			rn, err := c.rns.GetNodeRequest(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, fmt.Errorf("get node request: %w", err))
+			}
+			requestNodes = append(requestNodes, *rn)
+		case mnnode.NODE_KIND_FOR:
+			fn, err := c.fns.GetNodeFor(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, fmt.Errorf("get node for: %w", err))
+			}
+			forNodes = append(forNodes, *fn)
+		case mnnode.NODE_KIND_FOR_EACH:
+			fen, err := c.fens.GetNodeForEach(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, fmt.Errorf("get node for each: %w", err))
+			}
+			forEachNodes = append(forEachNodes, *fen)
+		case mnnode.NODE_KIND_NO_OP:
+			sn, err := c.sns.GetNodeNoop(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, fmt.Errorf("get node start: %w", err))
+			}
+			noopNodes = append(noopNodes, *sn)
+		case mnnode.NODE_KIND_CONDITION:
+			in, err := c.ins.GetNodeIf(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, errors.New("get node if"))
+			}
+			ifNodes = append(ifNodes, *in)
+		case mnnode.NODE_KIND_JS:
+			jsn, err := c.jsns.GetNodeJS(ctx, node.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, fmt.Errorf("get node js: %w", err))
+			}
+			jsNodes = append(jsNodes, jsn)
+		default:
+			return connect.NewError(connect.CodeInternal, errors.New("not supported node"))
+		}
+	}
+
+	var foundStartNode bool
+	for _, node := range noopNodes {
+		if node.Type == mnnoop.NODE_NO_OP_KIND_START {
+			if foundStartNode {
+				return connect.NewError(connect.CodeInternal, errors.New("multiple start nodes"))
+			}
+			foundStartNode = true
+			startNodeID = node.FlowNodeID
+		}
+	}
+	if !foundStartNode {
+		return connect.NewError(connect.CodeInternal, errors.New("no start node"))
+	}
+
+	flowNodeMap := make(map[idwrap.IDWrap]node.FlowNode, 0)
+	for _, forNode := range forNodes {
+		name := nodeNameMap[forNode.FlowNodeID]
+		flowNodeMap[forNode.FlowNodeID] = nfor.New(forNode.FlowNodeID, name, forNode.IterCount, time.Second)
+	}
+
+	requestNodeRespChan := make(chan nrequest.NodeRequestSideResp, len(requestNodes))
+	for _, requestNode := range requestNodes {
+
+		// Base Request
+		if requestNode.EndpointID == nil || requestNode.ExampleID == nil {
+			return connect.NewError(connect.CodeInternal, fmt.Errorf("endpoint or example not found for %s", requestNode.FlowNodeID))
+		}
+		endpoint, err := c.ias.GetItemApi(ctx, *requestNode.EndpointID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		example, err := c.es.GetApiExample(ctx, *requestNode.ExampleID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		if example.ItemApiID != endpoint.ID {
+			return connect.NewError(connect.CodeInternal, errors.New("example and endpoint not match"))
+		}
+		headers, err := c.hs.GetHeaderByExampleID(ctx, example.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, errors.New("get headers"))
+		}
+		queries, err := c.qs.GetExampleQueriesByExampleID(ctx, example.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, errors.New("get queries"))
+		}
+
+		rawBody, err := c.brs.GetBodyRawByExampleID(ctx, example.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		formBody, err := c.bfs.GetBodyFormsByExampleID(ctx, example.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		urlBody, err := c.bues.GetBodyURLEncodedByExampleID(ctx, example.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		exampleResp, err := c.ers.GetExampleRespByExampleID(ctx, example.ID)
+		if err != nil {
+			if err == sexampleresp.ErrNoRespFound {
+				exampleResp = &mexampleresp.ExampleResp{
+					ID:        idwrap.NewNow(),
+					ExampleID: example.ID,
+				}
+				err = c.ers.CreateExampleResp(ctx, *exampleResp)
+				if err != nil {
+					return connect.NewError(connect.CodeInternal, errors.New("create example resp"))
+				}
+			} else {
+				return connect.NewError(connect.CodeInternal, err)
+			}
+		}
+
+		exampleRespHeader, err := c.erhs.GetHeaderByRespID(ctx, exampleResp.ID)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, errors.New("get example resp header"))
+		}
+
+		asserts, err := c.as.GetAssertByExampleID(ctx, example.ID)
+		if err != nil && err != sassert.ErrNoAssertFound {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		// Delta Request
+		if requestNode.DeltaExampleID != nil {
+			deltaExample, err := c.es.GetApiExample(ctx, *requestNode.DeltaExampleID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, err)
+			}
+
+			deltaHeaders, err := c.hs.GetHeaderByExampleID(ctx, deltaExample.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, err)
+			}
+
+			deltaQueries, err := c.qs.GetExampleQueriesByExampleID(ctx, deltaExample.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, err)
+			}
+
+			rawBodyDelta, err := c.brs.GetBodyRawByExampleID(ctx, deltaExample.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, errors.New("delta raw body not found"))
+			}
+
+			formBodyDelta, err := c.bfs.GetBodyFormsByExampleID(ctx, deltaExample.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, errors.New("delta form body not found"))
+			}
+
+			urlBodyDelta, err := c.bues.GetBodyURLEncodedByExampleID(ctx, deltaExample.ID)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, errors.New("delta url body not found"))
+			}
+
+			mergeExamplesInput := request.MergeExamplesInput{
+				Base:  *example,
+				Delta: *deltaExample,
+
+				BaseQueries:  queries,
+				DeltaQueries: deltaQueries,
+
+				BaseHeaders:  headers,
+				DeltaHeaders: deltaHeaders,
+
+				BaseRawBody:  *rawBody,
+				DeltaRawBody: *rawBodyDelta,
+
+				BaseFormBody:  formBody,
+				DeltaFormBody: formBodyDelta,
+
+				BaseUrlEncodedBody:  urlBody,
+				DeltaUrlEncodedBody: urlBodyDelta,
+			}
+
+			mergeExampleOutput := request.MergeExamples(mergeExamplesInput)
+			example = &mergeExampleOutput.Merged
+
+			headers = mergeExampleOutput.MergeHeaders
+			queries = mergeExampleOutput.MergeQueries
+
+			rawBody = &mergeExampleOutput.MergeRawBody
+			formBody = mergeExampleOutput.MergeFormBody
+			urlBody = mergeExampleOutput.MergeUrlEncodedBody
+		}
+
+		httpClient := httpclient.New()
+
+		name := nodeNameMap[requestNode.FlowNodeID]
+
+		flowNodeMap[requestNode.FlowNodeID] = nrequest.New(requestNode.FlowNodeID, name, *endpoint, *example, queries, headers, *rawBody, formBody, urlBody,
+			*exampleResp, exampleRespHeader, asserts, httpClient, requestNodeRespChan)
+	}
+
+	for _, ifNode := range ifNodes {
+		comp := ifNode.Condition.Comparisons
+		name := nodeNameMap[ifNode.FlowNodeID]
+		flowNodeMap[ifNode.FlowNodeID] = nif.New(ifNode.FlowNodeID, name, comp.Kind, comp.Path, comp.Value)
+	}
+
+	for _, noopNode := range noopNodes {
+		name := nodeNameMap[noopNode.FlowNodeID]
+		flowNodeMap[noopNode.FlowNodeID] = nnoop.New(noopNode.FlowNodeID, name)
+	}
+
+	for _, forEachNode := range forEachNodes {
+		name := nodeNameMap[forEachNode.FlowNodeID]
+		flowNodeMap[forEachNode.FlowNodeID] = nforeach.New(forEachNode.FlowNodeID, name, forEachNode.IterPath, time.Second,
+			forEachNode.Condition, forEachNode.ErrorHandling)
+	}
+
+	var clientPtr *nodejs_executorv1connect.NodeJSExecutorServiceClient
+	for i, jsNode := range jsNodes {
+		if i == 0 {
+			client := nodejs_executorv1connect.NewNodeJSExecutorServiceClient(httpclient.New(), "http://localhost:9090")
+			clientPtr = &client
+		}
+
+		if jsNode.CodeCompressType != compress.CompressTypeNone {
+			jsNode.Code, err = compress.Decompress(jsNode.Code, jsNode.CodeCompressType)
+			if err != nil {
+				return connect.NewError(connect.CodeInternal, err)
+			}
+		}
+
+		name := nodeNameMap[jsNode.FlowNodeID]
+
+		flowNodeMap[jsNode.FlowNodeID] = njs.New(jsNode.FlowNodeID, name, string(jsNode.Code), *clientPtr)
+	}
+
+	// TODO: get timeout from flow config
+	runnerInst := flowlocalrunner.CreateFlowRunner(idwrap.NewNow(), latestFlowID, startNodeID, flowNodeMap, edgeMap, time.Second*10)
+
+	flowNodeStatusChan := make(chan runner.FlowNodeStatus, 1000)
+	flowStatusChan := make(chan runner.FlowStatus, 10)
+
+	subCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	done := make(chan error, 1)
+	updateNodeChan := make(chan mnnode.MNode, 1000)
+	go func() {
+		defer close(done)
+		nodeStatusFunc := func(flowNodeStatus runner.FlowNodeStatus) {
+			id := flowNodeStatus.NodeID
+			name := flowNodeStatus.Name
+			idStr := id.String()
+			stateStr := mnnode.StringNodeState(flowNodeStatus.State)
+			if flowNodeStatus.State != mnnode.NODE_STATE_RUNNING {
+				go func() {
+					ref := reference.NewReferenceFromInterfaceWithKey(flowNodeStatus, name)
+					refs := []reference.Reference{ref}
+
+					localErr := c.logChanMap.SendMsgToUserWithContext(ctx, idwrap.NewNow(), fmt.Sprintf("Node %s:%s: %s", name, idStr, stateStr), refs)
+					if localErr != nil {
+						done <- localErr
+						return
+					}
+				}()
+			}
+		}
+
+		for {
+			select {
+			case <-subCtx.Done():
+				close(flowNodeStatusChan)
+				close(flowStatusChan)
+				done <- errors.New("context done")
+				return
+			case flowNodeStatus, ok := <-flowNodeStatusChan:
+				if !ok {
+					return
+				}
+				nodeStatusFunc(flowNodeStatus)
+			case flowStatus, ok := <-flowStatusChan:
+				if !ok {
+					return
+				}
+				if len(flowNodeStatusChan) > 0 {
+					for flowNodeStatus := range flowNodeStatusChan {
+						nodeStatusFunc(flowNodeStatus)
+					}
+				}
+				if runner.IsFlowStatusDone(flowStatus) {
+					done <- nil
+					return
+				}
+			}
+		}
+	}()
+
+	flowRunErr := runnerInst.Run(ctx, flowNodeStatusChan, flowStatusChan)
+
+	// wait for the flow to finish
+	flowErr := <-done
+
+	close(updateNodeChan)
+	close(requestNodeRespChan)
+
+	if flowErr != nil {
+		return connect.NewError(connect.CodeInternal, err)
+	}
+
+	if flowRunErr != nil {
+		return connect.NewError(connect.CodeInternal, flowRunErr)
+	}
+
+	return nil
 }
