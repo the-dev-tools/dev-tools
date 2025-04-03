7ec4135cb1f071fb637da43489242711b407ac44
Tomas Zaluckij <me@tomaszal.com>
2025-04-03 15:28:26 +0100

    Implement Windows signing

--- apps/desktop/build.ts
+++ apps/desktop/build.ts
@@ -1,4 +1,6 @@
-import { pipe } from 'effect';
+import { Command } from '@effect/platform';
+import { NodeContext } from '@effect/platform-node';
+import { Config, Effect, pipe } from 'effect';
 import { build, type Configuration } from 'electron-builder';
 import { fileURLToPath } from 'node:url';
 
@@ -29,7 +31,37 @@ const config: Configuration = {
   },
   publish: { provider: 'custom' },
   win: {
-    signAndEditExecutable: false,
+    signtoolOptions: {
+      sign: (configuration) =>
+        pipe(
+          Effect.gen(function* () {
+            yield* pipe(
+              Command.make(
+                'azuresigntool',
+                'sign',
+                '--timestamp-rfc3161',
+                'http://timestamp.globalsign.com/tsa/advanced',
+                '--azure-key-vault-tenant-id',
+                yield* Config.string('AZURE_KEY_VAULT_TENANT_ID'),
+                '--azure-key-vault-url',
+                yield* Config.string('AZURE_KEY_VAULT_URL'),
+                '--azure-key-vault-client-id',
+                yield* Config.string('AZURE_KEY_VAULT_CLIENT_ID'),
+                '--azure-key-vault-client-secret',
+                yield* Config.string('AZURE_KEY_VAULT_CLIENT_SECRET'),
+                '--azure-key-vault-certificate',
+                yield* Config.string('AZURE_KEY_VAULT_CERTIFICATE'),
+                configuration.path,
+              ),
+              Command.stdout('inherit'),
+              Command.stderr('inherit'),
+              Command.exitCode,
+            );
+          }),
+          Effect.provide(NodeContext.layer),
+          Effect.runPromise,
+        ),
+    },
   },
 };
 
