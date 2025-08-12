53bbf794daad9fbfa9360a61a4a47a005cb78aeb
Tomas Zaluckij <me@tomaszal.com>
2025-08-12 15:27:44 +0100

    Implement model/operation/interface templating functionality in spec lib

--- configs/spec-lib/src/decorators.ts
+++ configs/spec-lib/src/decorators.ts
@@ -1,5 +1,45 @@
-import { DecoratorContext, EnumMember, Model, Operation, Scalar, StringLiteral, Type } from '@typespec/compiler';
-import { externals, maps, streams } from './state.js';
+import {
+  DecoratorContext,
+  EnumMember,
+  Interface,
+  Model,
+  Operation,
+  Scalar,
+  StringLiteral,
+  Type,
+} from '@typespec/compiler';
+import { $ } from '@typespec/compiler/typekit';
+import { pipe, Record } from 'effect';
+import { externals, maps, streams, templateInstances, templateNames, templates } from './state.js';
+
+const templateOf = ({ program }: DecoratorContext, template: Interface | Model | Operation, base: Model) => {
+  templates(program).add(template);
+
+  const name = base.name + template.name;
+
+  if (template.kind === 'Model') {
+    let instances = templateInstances(program).get(base);
+    instances ??= new Map();
+
+    let instance = instances.get(template);
+    instance ??= $(program).model.create({
+      name,
+      properties: pipe($(program).model.getProperties(template), Record.fromEntries),
+    });
+
+    templateInstances(program).set(base, instances);
+    instances.set(template, instance);
+  } else {
+    let names = templateNames(program).get(base);
+    names ??= new Map();
+
+    templateNames(program).set(base, names);
+    names.set(template, name);
+  }
+};
+
+const stream = ({ program }: DecoratorContext, target: Operation, mode: EnumMember) =>
+  streams(program).set(target, mode.name as never);
 
 const external = ({ program }: DecoratorContext, target: Model, path: StringLiteral, name: StringLiteral) =>
   externals(program).set(target, [path.value, name.value]);
@@ -7,10 +47,10 @@ const external = ({ program }: DecoratorContext, target: Model, path: StringLite
 const _map = ({ program }: DecoratorContext, target: Scalar, key: Type, value: Type) =>
   maps(program).set(target, [key, value]);
 
-const stream = ({ program }: DecoratorContext, target: Operation, mode: EnumMember) =>
-  streams(program).set(target, mode.name as never);
-
 export const $decorators = {
+  Lib: {
+    templateOf,
+  },
   TypeSpec: {
     stream,
   },
