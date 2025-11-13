4b2f3c08c5a300d396b7add2669b6d6304ab3732
Tomas Zaluckij <me@tomaszal.com>
2025-11-13 22:22:54 +0000

    Remove deprecated reorder callback handler

--- packages/ui/src/reorder.tsx
+++ packages/ui/src/reorder.tsx
@@ -1,39 +1,5 @@
-import { Array, Match, Option, pipe, Predicate } from 'effect';
 import { ElementType } from 'react';
-import { DroppableCollectionReorderEvent } from 'react-aria-components';
-import { MovePosition } from '@the-dev-tools/spec/resource/v1/resource_pb';
 import { tw } from './tailwind-literal';
-
-interface BasicReorderCallbackProps {
-  position: MovePosition;
-  source: string;
-  target: string;
-}
-
-export const basicReorder =
-  (callback: (props: BasicReorderCallbackProps) => void) =>
-  ({ keys, target: { dropPosition, key } }: DroppableCollectionReorderEvent) =>
-    Option.gen(function* () {
-      const position = yield* pipe(
-        Match.value(dropPosition),
-        Match.when('after', () => MovePosition.AFTER),
-        Match.when('before', () => MovePosition.BEFORE),
-        Match.option,
-      );
-
-      const source = yield* pipe(
-        yield* Option.liftPredicate(keys, (_) => _.size === 1),
-        Array.fromIterable,
-        Array.head,
-        Option.filter(Predicate.isString),
-      );
-
-      const target = yield* Option.liftPredicate(key, Predicate.isString);
-
-      if (source === target) return;
-
-      callback({ position, source, target });
-    });
 
 interface DropIndicator {
   as?: ElementType;
