d08ef7caef616f716837afd725d667de7108eac9
Tomas Zaluckij <me@tomaszal.com>
2026-01-07 13:35:18 +0000

    Move all packages to PNPM catalog to improve dependency management

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -1,29 +1,6 @@
 /** @type {import("syncpack").RcFile} */
 export default {
   dependencyTypes: ['!local'],
-  semverGroups: [
-    {
-      dependencies: ['**'],
-      dependencyTypes: ['!dev', '!peer'],
-      label: 'Production dependencies should have fixed version numbers',
-      packages: ['**'],
-      range: '',
-    },
-    {
-      dependencies: ['**'],
-      dependencyTypes: ['dev'],
-      label: 'Development dependencies should have fixed minor version',
-      packages: ['**'],
-      range: '~',
-    },
-    {
-      dependencies: ['**'],
-      dependencyTypes: ['peer'],
-      label: 'Peer dependencies should have fixed major version',
-      packages: ['**'],
-      range: '^',
-    },
-  ],
   sortFirst: [
     'name',
     'displayName',
